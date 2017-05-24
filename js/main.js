67226d6aabc165e64dc68bf010038f803583bdb8
Susana Lozano <susanalozanoadrio@gmail.com>
2017-05-24 16:17:47 +0200

    html css sass y js ok

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 var button = document.querySelector(".btnOpenMenu")
 
 function myToggle() {
-var list = document.getElementById("myMenuMobile");
-    if (list.style.display === "none"||list.style.display ==="") {
-        list.style.display = "block";
-    } else {
-        list.style.display = "none";
-    }
+  var list = document.getElementById("myMenuMobile");
+  if (list.style.display === "none" || list.style.display === "") {
+    list.style.display = "block";
+  } else {
+    list.style.display = "none";
+  }
 
 }
-button.addEventListener("click",myToggle);
+button.addEventListener("click", myToggle);
